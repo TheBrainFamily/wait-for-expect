0f11a85f704561b98d87b8039855c925b422766d
Valentin Ignatev <valentignatev@gmail.com>
2018-11-16 11:54:18 +0300

    feat: use real Date.now to avoid using potentially mocked one (#12)
    * (#12) use real Date.now to avoid using potentially mocked one
    
    * (#12) make tsc happy with Date on Window interface and Date jest mock

--- src/withFakeTimers.spec.ts
+++ src/withFakeTimers.spec.ts
@@ -2,9 +2,14 @@
 import "./toBeInRangeMatcher";
 import waitForExpect from "./index";
 
-// this is a copy of "it waits for expectation to pass" modified to use jestFakeTimers
-// This breakes when we remove the const { setTimeout } = typeof window !== "undefined" ? window : global;
+// this is a copy of "it waits for expectation to pass" modified to use jestFakeTimers and two ways of Date.now mocking
+// This breakes when we remove the const { setTimeout, Date: { now } } = typeof window !== "undefined" ? window : global;
 // line from the index.ts
+
+beforeEach(() => {
+  jest.restoreAllMocks();
+  jest.useRealTimers();
+});
 
 test("it works even if the timers are overwritten by jest", async () => {
   jest.useFakeTimers();
@@ -21,4 +26,56 @@ test("it works even if the timers are overwritten by jest", async () => {
   await waitForExpect(() => {
     expect(numberToChange).toEqual(100);
   });
+});
+
+// Date.now might be mocked with two main ways:
+// via mocking whole Date, or by mocking just Date.now
+// hence two test cases covered both ways
+test("it works even if the Date was mocked", async () => {
+  /* eslint-disable no-global-assign */
+  // @ts-ignore: Cannot reassign to const Date
+  Date = jest.fn(() => ({
+    now() {
+      return 1482363367071;
+    }
+  }));
+  /* eslint-enable */
+  let numberToChange = 10;
+
+  setTimeout(() => {
+    numberToChange = 100;
+  }, 100);
+  let expectFailingMessage;
+  try {
+    await waitForExpect(() => {
+      expect(numberToChange).toEqual(101);
+    }, 1000);
+  } catch (e) {
+    expectFailingMessage = e.message;
+  }
+  expect(expectFailingMessage).toMatch("Expected value to equal:");
+  expect(expectFailingMessage).toMatch("101");
+  expect(expectFailingMessage).toMatch("Received:");
+  expect(expectFailingMessage).toMatch("100");
+});
+
+test("it works even if the Date.now was mocked", async () => {
+  Date.now = jest.fn(() => 1482363367071);
+  let numberToChange = 10;
+
+  setTimeout(() => {
+    numberToChange = 100;
+  }, 100);
+  let expectFailingMessage;
+  try {
+    await waitForExpect(() => {
+      expect(numberToChange).toEqual(101);
+    }, 1000);
+  } catch (e) {
+    expectFailingMessage = e.message;
+  }
+  expect(expectFailingMessage).toMatch("Expected value to equal:");
+  expect(expectFailingMessage).toMatch("101");
+  expect(expectFailingMessage).toMatch("Received:");
+  expect(expectFailingMessage).toMatch("100");
 });
