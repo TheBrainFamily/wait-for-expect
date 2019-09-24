dd272db889cac5b408f5b1d8bb91fb2ed911d236
Łukasz Gandecki <4002543+lgandecki@users.noreply.github.com>
2019-09-24 17:04:40 +0200

    feat: Add runWithRealTimers & getSetTimeoutFn functions
    * Add runWithRealTimers & getSetTimeoutFn functions
    
    BREAKING CHANGE: If you are using fake jest timers by default, but upgraded to v2.0 here, you might have to upgrade your tests. Sorry! :(
    
    
    Co-authored-by: Brais Piñeiro <brapifra@gmail.com>

---
+++ src/helpers.ts
@@ -0,0 +1,30 @@
+/* eslint-disable import/prefer-default-export */
+/* eslint-env jest */
+// Used to avoid using Jest's fake timers and Date.now mocks
+// See https://github.com/TheBrainFamily/wait-for-expect/issues/4 and
+// https://github.com/TheBrainFamily/wait-for-expect/issues/12 for more info
+const globalObj = typeof window === "undefined" ? global : window;
+
+// Currently this fn only supports jest timers, but it could support other test runners in the future.
+function runWithRealTimers(callback: () => any) {
+  const usingJestFakeTimers =
+    // eslint-disable-next-line no-underscore-dangle
+    (globalObj.setTimeout as any)._isMockFunction &&
+    typeof jest !== "undefined";
+
+  if (usingJestFakeTimers) {
+    jest.useRealTimers();
+  }
+
+  const callbackReturnValue = callback();
+
+  if (usingJestFakeTimers) {
+    jest.useFakeTimers();
+  }
+
+  return callbackReturnValue;
+}
+
+export function getSetTimeoutFn() {
+  return runWithRealTimers(() => globalObj.setTimeout);
+}
