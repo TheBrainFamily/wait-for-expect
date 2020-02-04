0c6b2fd840e8f985d8c8e59c1f81edcacc73c962
René van Mil <rvanmil@me.com>
2020-02-04 16:13:44 +0100

    fix prettier issues

--- src/waitForExpect.spec.ts
+++ src/waitForExpect.spec.ts
@@ -22,43 +22,35 @@ test("it waits for expectation to pass", async () => {
   });
 });
 
-test(
-  "it fails properly with jest error message when it times out without expectation passing",
-  async done => {
-    const numberNotToChange = 200;
-    try {
-      await waitForExpect(() => {
-        expect(numberNotToChange).toEqual(2000);
-      }, 300);
-    } catch (e) {
-      expect(e.message).toMatchSnapshot();
-      done();
-    }
-  },
-  1000
-);
+test("it fails properly with jest error message when it times out without expectation passing", async done => {
+  const numberNotToChange = 200;
+  try {
+    await waitForExpect(() => {
+      expect(numberNotToChange).toEqual(2000);
+    }, 300);
+  } catch (e) {
+    expect(e.message).toMatchSnapshot();
+    done();
+  }
+}, 1000);
 
-test(
-  "it fails when the change didn't happen fast enough, based on the waitForExpect timeout",
-  async done => {
-    let numberToChangeTooLate = 300;
-    const timeToPassForTheChangeToHappen = 1000;
+test("it fails when the change didn't happen fast enough, based on the waitForExpect timeout", async done => {
+  let numberToChangeTooLate = 300;
+  const timeToPassForTheChangeToHappen = 1000;
 
-    setTimeout(() => {
-      numberToChangeTooLate = 3000;
-    }, timeToPassForTheChangeToHappen);
+  setTimeout(() => {
+    numberToChangeTooLate = 3000;
+  }, timeToPassForTheChangeToHappen);
 
-    try {
-      await waitForExpect(() => {
-        expect(numberToChangeTooLate).toEqual(3000);
-      }, timeToPassForTheChangeToHappen - 200);
-    } catch (e) {
-      expect(e.message).toMatchSnapshot();
-      done();
-    }
-  },
-  1500
-);
+  try {
+    await waitForExpect(() => {
+      expect(numberToChangeTooLate).toEqual(3000);
+    }, timeToPassForTheChangeToHappen - 200);
+  } catch (e) {
+    expect(e.message).toMatchSnapshot();
+    done();
+  }
+}, 1500);
 
 test("it reruns the expectation every x ms, as provided with the second argument", async () => {
   // using this would be preferable but somehow jest shares the expect.assertions between tests!
